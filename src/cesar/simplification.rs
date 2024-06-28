56960f731ec386544b052989a92e1f86e2216877
Ravneet Singh Bhatia <ravneet@vibes-playground>
2024-06-28 10:41:33 -0700

    creating get_runner function

--- src/cesar/simplification.rs
+++ src/cesar/simplification.rs
@@ -8,7 +8,22 @@ trait Simplification {
     
     fn make_rules() -> Vec<Rewrite, <PropLang, ()>>;
 
-    fn simplify(problem: String, assumptions: String) -> String {
+    fn get_runner(has_node_limit: bool) -> Runner<PropLang, ()> {
+        // create default runner.
+        let runner = Runner<PropLang, ()>::default();
+
+        if has_node_limit {
+            runner.with_time_limit(std::time::Duration::from_secs(config::TIMEOUT * 3))
+                .with_node_limit(100_000)
+                .with_iter_limit(100_000)
+        } else {
+            runner.with_time_limit(std::time::Duration::from_secs(config::TIMEOUT))
+        }
+
+    }
+
+
+    fn simplify(problem: String, assumptions: String, has_node_limit) -> String {
         unsafe {ASSUMPTIONS = assumptions};
         
         // Parse the problem, the assumptions, and the rules.
@@ -16,7 +31,8 @@ trait Simplification {
         let rules = Self::make_rules();
 
         // Run the rules
-        // TODO Let runner = getRunner(); 
+        let runner = getRunner(has_node_limit).with_explanations_enabled()
+            .with_expr(&problem).run(&rules);
         
         // Extract the best expression
         let Extractor = Extractor::new(&runner.egraph, AstSize);
