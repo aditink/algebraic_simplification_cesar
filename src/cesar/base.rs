3fb7226749909fa8963afeedae4a4184e0dc0ff4
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-07-31 10:31:13 -0700

    implementing simplify

--- src/cesar/base.rs
+++ src/cesar/base.rs
@@ -1,27 +1,55 @@
 use crate::cesar::config;
 use crate::cesar::language::PropLang;
 use egg::*;
+use log::debug;
+use log::info;
+use std::time::Duration;
 
-/// The function returns a new runner with the required properties.
+/// Helper function to return a new runner with the required properties.
 ///
 /// # Parameters
 ///
 /// - 'has_node_limit': Determines if the returned runner needs to have an iter limit and a node
 /// limit. Both limits are 100_000.
 ///
+/// - 'timeout': The timeout for the runner. The runner will stop when it has been running for
+/// timeout seconds regardless of its progress. Must be a positive `u64` value.
+///
 /// # Returns
 ///
 /// A new Runner to simplify the problem.
-fn get_runner(has_node_limit: bool) -> Runner<PropLang, ()> {
+fn get_runner(has_node_limit: bool, timeout: u64) -> Runner<PropLang, ()> {
     // Default runner
     let runner = Runner::<PropLang, ()>::default();
 
     if has_node_limit {
         runner.with_node_limit(100_000).with_iter_limit(100_000)
-    } else {
-        runner
     }
+
+    runner
+        .with_time_limit(Duration::from_secs(timeout))
+        .with_explanations_enabled()
 }
+
+/// This function simplifes a problem, given the rules for simplification.
+///
+/// # Parameters
+///
+/// - 'problem': The formula to be simplified. Must be a `String` value.
+///
+/// - 'has_node_limit': Determines if the returned runner needs to have an iter limit and a node
+/// limit. Both limits are 100_000.
+///
+/// - 'timeout': The timeout for the runner. The runner will stop when it has been running for
+/// timeout seconds regardless of its progress. Must be a positive `u64` value.
+///
+/// - 'rules': The rules for simplification. These rules are generated in their respective passes.
+/// Must be a `Vec<Rewrite<PropLang, ()>>`.
+///
+/// # Returns
+///
+/// A `String` of the simplifed formula.
+///
 
 pub fn simplify(
     problem: String,
@@ -29,4 +57,34 @@ pub fn simplify(
     timeout: u64,
     rules: Vec<Rewrite<PropLang, ()>>,
 ) -> String {
+    debug!(
+        "Running simplify with has_node_limit set to {}",
+        has_node_limit
+    );
+
+    let problem = problem.parse().unwrap();
+
+    // Get the runner with the needed properties.
+    let mut runner = get_runner(has_node_limit, timeout);
+
+    // Simplify the problem using the given rules.
+    runner.with_expr(&problem).run(&rules);
+
+    // Helps to extract the most simplifed formula from the graph.
+    let extractor = Extractor::new(&runner.egraph, AstSize);
+    let simplified = extractor.find_best(runner.roots[0]);
+
+    debug!("Simplification finished.");
+    debug!("Original Problem: {}", problem);
+    debug!("Simplified Problem: {}", simplified.1);
+    debug!("Cost of simplification: {}", simplifed.0);
+    debug!("Runner stop reason: {}", runner.stop_reason);
+
+    let explanation = runner
+        .explain_equivalence(&problem, &simplified.1)
+        .get_flat_string();
+    debug!("Explanations: {}, explanation");
+
+    // Return the simplifed problem as a String.
+    simplified.1.to_string()
 }
